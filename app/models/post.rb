b45257f89063a560e93209ecc2e54fc1becce8e3
SilverFox70 <wfbrinkert@gmail.com>
2016-04-27 09:59:54 -0700

    Fix bug in pager that was preventing paging through gaps in ID within the blog posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ActiveRecord::Base
-	has_many :comments, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
+  def previous_two_id(id)
+    first_prev = self.class.where("id < ?", id).last.id
+    self.class.where("id < ?", id).last.id
+  end
 end
